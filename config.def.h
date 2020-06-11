0b9875eda33e0211408ed87db96c7f57c340d6f7
Vitaly <blackjack358@gmail.com>
2020-06-11 19:11:46 +0300

    Change default sizes.

--- config.def.h
+++ config.def.h
@@ -148,8 +148,8 @@ static unsigned int cursorshape = 2;
  * Default columns and rows numbers
  */
 
-static unsigned int cols = 80;
-static unsigned int rows = 24;
+static unsigned int cols = 90;
+static unsigned int rows = 25;
 
 /*
  * Default colour and shape of the mouse cursor
